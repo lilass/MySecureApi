ea46f35c9c20e6b5a284f736c7b884a38e684be0
lilass <71955348+lilass@users.noreply.github.com>
2020-12-14 14:13:15 -0500

    First Committ

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MySecureApi.Models;
+
+namespace MySecureApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly InventoryContext _context;
+
+        public ProductsController(InventoryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/(ALL)Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Products>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        // GET: api/(ONE)Products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Products>> GetProducts(int id)
+        {
+            var products = await _context.Products.FindAsync(id);
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return products;
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public async Task<ActionResult<Products>> PostProducts(Products products)
+        {
+            _context.Products.Add(products);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProducts", new { id = products.ProductId }, products);
+        }
+
+
+
+        private bool ProductsExists(int id)
+        {
+            return _context.Products.Any(e => e.ProductId == id);
+        }
+    }
+}
